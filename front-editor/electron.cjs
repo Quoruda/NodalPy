25d69dcfa0ec73143eb2ac969ab289228e4a1c41
Quoruda <audrick.soltner@proton.me>
2025-07-23 22:09:25 +0200

    Add Electron

---
+++ front-editor/electron.cjs
@@ -0,0 +1,31 @@
+// electron.js
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  // En développement : charger le serveur Vite
+  win.loadURL('http://localhost:5173');
+
+  // En production : charger les fichiers buildés
+  // win.loadFile('dist/index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
