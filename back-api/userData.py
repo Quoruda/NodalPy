8c0fa4bb9fcd94d3fea806f7579fde371a645c72
Quoruda <audrick.soltner@proton.me>
2025-10-18 21:55:34 +0200

    Update UserData

--- back-api/userData.py
+++ back-api/userData.py
@@ -1,18 +1,18 @@
 import copy
-import sys
-from io import StringIO
 from runFunctions import *
 
-
-
 class UserData:
-    def __init__(self, userId: str):
-        self.userId = userId
+    def __init__(self, identifier: str):
+        self.userId = identifier
         self.nodeContexts = {}
         self.thread = None
 
     def can_run_code(self):
         return self.thread is None or not self.thread.is_alive()
+
+    def get_variable(self, node: str, name: str):
+        node_context = self.nodeContexts.get(node, {})
+        return node_context.get(name, None)
 
     def run_node(self, node: str, code: str, variables: list[dict]):
         if not self.can_run_code():
@@ -30,8 +30,10 @@ class UserData:
         exec_globals, local_scope = prepare_contexts(new_context)
         self.nodeContexts[node] = local_scope
 
-        self.thread = run_code_in_thread(code, exec_globals, local_scope)
-        self.thread.join()
+        try:
+            exec(code, exec_globals, local_scope)
+        except Exception as e:
+            pass
 
         return local_scope["__stdout__"].getvalue() if "__stdout__" in local_scope else ""
 
