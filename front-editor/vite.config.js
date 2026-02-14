b59a897392055802dec28134ebb527c93057b598
Quoruda <audrick.soltner@proton.me>
2026-02-14 18:58:41 +0100

    Enhance Vite configuration by adding manual chunking for vendor libraries and adjusting chunk size warning limit for improved build performance

--- front-editor/vite.config.js
+++ front-editor/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          flow: ['@xyflow/react'],
+          codemirror: ['@uiw/react-codemirror', '@codemirror/lang-python'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
 })
